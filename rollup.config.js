cfa1ef839df3f8b37be875e2dda64c751e546aca
Chuanqi Sun <chusun@microsoft.com>
2017-05-31 18:00:48 -0700

    rename main file

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
 export default {
-    entry: 'dist/contrast-matrix.js',
+    entry: 'dist/main.js',
     format: 'iife',
     plugins: [ 
         resolve(),
