de36512286eb92a9ece74fe6a65d74275a3891ee
Chuanqi Sun <chusun@microsoft.com>
2017-05-31 17:33:40 -0700

    split controller and view into their own files

---
+++ src/controller.ts
@@ -0,0 +1,54 @@
+import { Color, FilePicker, JsonFileParser } from 'visually';
+import { Model } from './model';
+import { View } from './view';
+    
+export class Controller {
+    private addBackground = document.getElementsByClassName('editor__add-background')[0];
+    private addForeground = document.getElementsByClassName('editor__add-foreground')[0];
+    private colorPicker = document.getElementsByClassName('editor__color-picker')[0] as HTMLInputElement;
+    private inputElement = document.getElementsByClassName('loader')[0];
+    private picker = new FilePicker(this.inputElement as HTMLInputElement);
+    private parser = new JsonFileParser();
+    private backgroundNewCounter = 0;
+    private foregroundNewCounter = 0
+
+    constructor(private model: Model, private view: View) {
+        this.handleInput();
+        this.view.render();
+    }
+
+    private handleInput(): void {
+        this.addBackground.addEventListener('click', () => {
+            try { 
+                this.model.backgrounds.unshift({
+                    name: "background " + ++this.backgroundNewCounter,
+                    color: new Color(this.colorPicker.value)
+                });
+                this.view.render();
+            } catch (e) {
+                window.alert('invalid color value');
+            }
+        });
+
+        
+        this.addForeground.addEventListener('click', () => {
+            try {
+                this.model.foregrounds.unshift({
+                    name: "foreground " + ++this.foregroundNewCounter,
+                    color: new Color(this.colorPicker.value)
+                });
+                this.view.render();
+            } catch (e) {
+                window.alert('invalid color value');
+            }
+        });
+
+        this.picker.filesPicked.subscribe(file => this.parser.parseFile(file[0]));
+        this.parser.fileParsed.subscribe(object => {
+            this.model.backgrounds = object.backgrounds.map((item: any) => ({ name: item.name, color: new Color(item.value)}));
+            this.model.foregrounds = object.foregrounds.map((item: any) => ({ name: item.name, color: new Color(item.value)}));
+            this.view.render();
+            this.picker.reset();
+        });
+    }
+}
